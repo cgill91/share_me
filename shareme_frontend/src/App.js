3014f5e7754c32daf6718e94ef35107dcc4ef74e
Colton <coltongillmore09@gmail.com>
2022-10-25 19:31:39 -0500

    navbar

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import {Routes, Route, useNavigate } from 'react-router-dom';
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
-import Login from './components/Login';
-import Home from './containers/Home';
+import Login from "./components/Login";
+import Home from "./containers/Home";
 
 const App = () => {
   return (
     <Routes>
       <Route path="login" element={<Login />} />
-      <Route path="/*" element={<Home />} /> 
+      <Route path="/*" element={<Home />} />
     </Routes>
-  )
-}
+  );
+};
 
 export default App;
